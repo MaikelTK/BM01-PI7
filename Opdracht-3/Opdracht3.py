efc9015be0d0128569fd244ed1ee1f8ca36d21a7
MaikelTK <maikel.ten.koppel@gmail.com>
2019-10-28 09:21:54 +0100

    added bigram functions

--- Opdracht-3/Opdracht3.py
+++ Opdracht-3/Opdracht3.py
@@ -1,25 +1,20 @@
-from string import punctuation, digits, ascii_letters
+from string import punctuation, digits
 import re as re
-#from nltk import word_tokenize
 from nltk.tokenize import RegexpTokenizer
-#from nltk.util import ngrams
 import pandas as pd
 
-data_url = 'Opdracht-3/traning_data/Bible-American_Standard_Version.txt'
-
-columns = ['ngram', 'EN', 'DE', 'ES', 'FR', 'IT']
-
-ngram_model = pd.DataFrame(columns=columns)
-
 # Load dataset
-def load_dataset(url):
+def load_dataset(url):    
     file = open(url, 'rt')
     data = file.read()
     file.close()
+    limit = 100000
 
-    print(len(data))
-
-    #data = data[:1000000]
+    print('Read file: ', url)
+    print('File len: ', len(data))
+    print('Using: ', limit, '/', len(data))
+    data = data[limit:(limit *2)]
+    
     return data
 
 # Remove al chars except the alphabet
@@ -28,7 +23,8 @@ def clean_dataset(dataset):
     words = words.lower()
     tokenizer = RegexpTokenizer(r"\w+") # Remove everything but words
     words = tokenizer.tokenize(words) # Put words in list
-    
+    print('Removing digits and punctuation')
+    print(len(words), 'words found')
     return words
 
 # Fill dataframe with ngrams
@@ -36,29 +32,98 @@ def create_ngram(text, n, language, df):
     text.sort()
     x = (len(text) / 10)
     counter = 0
-
+    percentage = 0
+    print('Creating bigrams...')
 
     for word in text:
+        
         counter += 1
         if counter >= x:
-            print('+ 10%')
+            percentage += 1
+            print((percentage * 10), '% Done...')
             counter = 0
+
         for i in range(len(word)):
             ngram = (word[i:i+n])
+            if ngram not in df.values:
+                df = df.append({'ngram': ngram, 'EN': 0, 'DE': 0, 'ES': 0, 'FR': 0, 'IT': 0, 'PO': 0  }, ignore_index=True)
             if ngram in df.values:
                 row = df.loc[df['ngram'] == ngram] #locate row of 
                 df.loc[row.index, language] += 1
-            else:
-                df = df.append({'ngram': ngram, language : 1 }, ignore_index=True)
+                
     print (df.sort_values(by=[language], ascending=False))
     return df
 
-# Get dataset
-dataset = load_dataset(data_url)
-clean_data = clean_dataset(dataset)
-#print(clean_data)
-ngram_model = create_ngram(clean_data, 2, 'EN', ngram_model)
-ngram_model.to_csv(r'export_bigram.csv', index=None, header=True)
+def create_empty_dataframe():
+    columns = ['ngram', 'EN', 'DE', 'ES', 'FR', 'IT', 'PO']
+    df = pd.DataFrame(columns=columns)
+    df.to_csv(r'export_bigram-Bible.csv', index=None, header=True)
 
 
+def main(url, language, dataframe):
+    # Load text dataset
+    dataset = load_dataset(url)
+    
+    # Clean the dataset
+    clean_data = clean_dataset(dataset)
 
+    # Get ngram dataframe from text
+    ngram_model = create_ngram(clean_data, 2, language, dataframe)
+
+    print('Ngram dataframe: ', ngram_model)
+    
+    # Save dataframe as csv
+    ngram_model.to_csv(r'export_bigram-Bible.csv', index=None, header=True)
+
+# ----------------------------------------------------------------------------- #
+
+def create_ngram_model():
+
+    languages = [
+    ['DE', 'Opdracht-3/traning_data/Bible_DE.txt'], # German
+    ['EN', 'Opdracht-3/traning_data/Bible_EN.txt'], # English
+    ['FR', 'Opdracht-3/traning_data/Bible_FR.txt'], # French
+    ['ES', 'Opdracht-3/traning_data/Bible_ES.txt'], # Spanish
+    ['PO', 'Opdracht-3/traning_data/Bible_PO.txt'], # Portuguese
+    ['IT', 'Opdracht-3/traning_data/Bible_IT.txt'] # Italian
+    ]
+
+    df = pd.read_csv('export_bigram-Bible.csv')
+    print(df)
+
+    print('0:DE | 1:EN | 2:FR | 3:ES | 4:PO | 5:IT')
+    number = input('Select language 0-5: ')
+    num = int(number)
+    language = languages[num] 
+    lan = language[0]
+    text = language[1]
+
+    main(text, lan, df)
+
+
+def detect_language(n):
+    columns = ['EN', 'DE', 'ES', 'FR', 'IT', 'PO']
+    points = [0, 0, 0, 0, 0, 0]
+
+    df = pd.read_csv('export_bigram-Bible.csv')
+
+    text = 'I have no idea what I am doing'
+    text = clean_dataset(text)
+    print(text)
+
+    for word in text:
+        for i in range(len(word)):
+            ngram = (word[i:i+n])
+            if ngram in df.values:
+                row = (df.loc[df['ngram'] == ngram].index) #locate row
+                index = row[0]
+                x = df.loc[index]
+                print(x)
+                # Get column name
+                # give point to highest column
+                
+detect_language(2)
+
+
+#create_empty_dataframe()
+#create_ngram_model()
